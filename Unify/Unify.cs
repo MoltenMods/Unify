f6ed74779c851011af3de4aa5774ae97c878f986
DaemonBeast <DaemonBeast@outlook.com>
2021-02-10 21:32:26 +0000

    In the beginning, there was nothing...

---
+++ Unify/Unify.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using BepInEx.IL2CPP;
+using HarmonyLib;
+using Reactor;
+
+namespace Unify
+{
+    [BepInPlugin(Id, Name, Version)]
+    [BepInProcess("Among Us.exe")]
+    [BepInDependency(ReactorPlugin.Id)]
+    public class UnifyPlugin : BasePlugin
+    {
+        public const string Id = "reactor.unify";
+        private const string Name = "Unify";
+        private const string Version = "0.1.0";
+
+        public Harmony Harmony { get; } = new Harmony(Id);
+
+        public override void Load()
+        {
+            Harmony.PatchAll();
+        }
+    }
+}
