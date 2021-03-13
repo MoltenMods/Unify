80051f9ec23c818bb85e5edcf7c3dcdf00bc3182
DaemonBeast <DaemonBeast@outlook.com>
2021-03-13 15:20:06 +0000

    Removed unused `using` directives

--- Unify/Patches/Regions.cs
+++ Unify/Patches/Regions.cs
@@ -1,9 +1,5 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using BepInEx.Configuration;
-using BepInEx.Preloader.Core;
-using HarmonyLib;
-using UnityEngine;
 using Array = System.Array;
 using String = System.String;
 
