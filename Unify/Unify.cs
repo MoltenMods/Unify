3dbf2befc1689b3e7ee18d984bec2e02d31af0a6
DaemonBeast <DaemonBeast@outlook.com>
2021-03-14 12:40:21 +0000

    Revert "Modified mod to remove Reactor as a dependency"
    This reverts commit af6e7f4b4cda2aee8b0ce18fe0ca39e4d70df429.

--- Unify/Unify.cs
+++ Unify/Unify.cs
@@ -3,17 +3,19 @@ using BepInEx;
 using BepInEx.Configuration;
 using BepInEx.IL2CPP;
 using HarmonyLib;
+using Reactor;
 using Unify.Patches;
 
 namespace Unify
 {
     [BepInPlugin(Id, Name, Version)]
     [BepInProcess("Among Us.exe")]
+    [BepInDependency(ReactorPlugin.Id)]
     public class UnifyPlugin : BasePlugin
     {
         public const string Id = "daemon.unify.reactor";
         private const string Name = "Unify";
-        private const string Version = "2.0.2";
+        private const string Version = "2.0.1";
         
         public static readonly ConfigFile ConfigFile =
             new ConfigFile(Path.Combine(Paths.ConfigPath, $"{UnifyPlugin.Id}.cfg"), true);
@@ -22,6 +24,8 @@ namespace Unify
 
         public override void Load()
         {
+            RegionsPatch.Patch();
+            
             Harmony.PatchAll();
         }
     }
